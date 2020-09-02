b4ad3a177fe3ce4d321a5550a8fd98e9020f8989
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-09-02 17:01:16 -0400

    Done.

--- test/index.js
+++ test/index.js
@@ -3,10 +3,10 @@ var expect = require('chai').expect,
   semver = require('semver')
 
 describe('node version', function () {
-  it('must be 5.1', function(done){
-    expect(process.versions.node).to.equal('5.1.0')
-    done()
-  })
+  it("must be 12.18.3", function (done) {
+    expect(process.versions.node).to.equal("12.18.3");
+    done();
+  });
 
 })
 
