6fc6ad4e995bbc7b7013dc28de52eec821bb8999
Nashmeyah <nashalrekabi@gmail.com>
2021-03-03 01:21:27 -0700

    Done.

--- test/index.js
+++ test/index.js
@@ -1,26 +1,24 @@
-var expect = require('chai').expect,
-  cp = require('child_process'),
-  semver = require('semver')
+var expect = require("chai").expect,
+  cp = require("child_process"),
+  semver = require("semver");
 
-describe('node version', function () {
-  it('must be 5.1', function(done){
-    expect(process.versions.node).to.equal('5.1.0')
-    done()
-  })
+describe("node version", function () {
+  it("must be 14.7.0", function (done) {
+    expect(process.versions.node).to.equal("14.7.0");
+    done();
+  });
+});
 
-})
-
-describe('npm version', function () {
-  it('must be 2.14.15 or greater', function(done){
-    child = cp.exec('npm -v',
-    function (error, stdout, stderr) {
-      expect(stderr).to.equal('')
+describe("npm version", function () {
+  it("must be 2.14.15 or greater", function (done) {
+    child = cp.exec("npm -v", function (error, stdout, stderr) {
+      expect(stderr).to.equal("");
       if (error !== null) {
-        console.log('exec error: ' + error)
+        console.log("exec error: " + error);
       }
-      stdout = stdout.replace('\n','')
-      expect(semver.satisfies(stdout, '>=2.14.15')).to.equal(true)
-      done()
-    })
-  })
-})
+      stdout = stdout.replace("\n", "");
+      expect(semver.satisfies(stdout, ">=2.14.15")).to.equal(true);
+      done();
+    });
+  });
+});
